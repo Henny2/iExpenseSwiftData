278f7ea7321d59dc0473a556355f77bd8211ef16
Henny2 <henrieke.baunack@web.de>
2023-12-19 20:01:16 -0800

    Initial Commit

---
+++ iExpenseSwiftData/iExpenseSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseSwiftDataApp.swift
+//  iExpenseSwiftData
+//
+//  Created by Henrieke Baunack on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
