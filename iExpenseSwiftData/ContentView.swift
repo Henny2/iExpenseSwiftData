bad3415bb03f3b38ba321acf3006b357dd67a589
Henny2 <henrieke.baunack@web.de>
2023-12-22 20:25:12 -0800

    add dynamic filtering

--- iExpenseSwiftData/ContentView.swift
+++ iExpenseSwiftData/ContentView.swift
@@ -26,92 +26,37 @@ class ExpenseItem {
 // https://www.hackingwithswift.com/quick-start/swiftdata/sorting-query-results
 // explains that you need to create a subview to change parameters of the query
  
-
 struct ContentView: View {
-   
-    
-    //    @Query(
-    //        filter: #Predicate<User> { user in
-    //            user.name.localizedStandardContains("r") &&
-    //            user.city == "Liverpool" },
-    //        sort: \User.name) var users: [User]
-    
     @Environment(\.modelContext) var modelContext
     @State var sortOrder = [SortDescriptor(\ExpenseItem.name)]
-//    @Query(sort: sortOrder) var expenses: [ExpenseItem]
-    @Query var expenses: [ExpenseItem]
-    // using this as condition for showing a sheet
     @State private var showingAddExpense = false
     var body: some View {
         NavigationStack{
-            List{
-                // don't need the id: \.id anymore because we use the Identifiable protocol, Swift knows there is a unique id
-                Section("Personal Expenses"){
-                    ForEach(expenses){ item in
-                        if item.type == "Personal" {
-                            HStack{
-                                VStack(alignment: .leading) {
-                                    Text(item.name).font(.headline)
-                                    Text(item.type)
-                                }
-                                Spacer()
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                                    .fontWeight(item.amount < 10.0 ? .light: item.amount < 100 ? .bold : .heavy)
-                            }
+            ExpesnseListView(sort: sortOrder)
+                .navigationTitle("iExpense")
+                .toolbar {
+                    Button("Add expense", systemImage: "plus"){
+                        showingAddExpense = true
+                    }
+                    Menu("Filter", systemImage: "arrow.up.arrow.down"){
+                        Picker("Pick filter", selection: $sortOrder){
+                            Text("By Name")
+                                .tag([SortDescriptor(\ExpenseItem.name)])
+                            Text("By Amount")
+                                .tag([SortDescriptor(\ExpenseItem.amount)])
+                            
                         }
                     }
-                    .onDelete(perform: removeItems)
+
                 }
-                Section("Business Expenses"){
-                    ForEach(expenses){ item in
-                        if item.type == "Business"{
-                            HStack{
-                                VStack(alignment: .leading) {
-                                    Text(item.name).font(.headline)
-                                    Text(item.type)
-                                }
-                                Spacer()
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                                    .fontWeight(item.amount < 10.0 ? .light: item.amount < 100 ? .bold : .heavy)
-                            }
-                        }
-                    }
-                    .onDelete(perform: removeItems)
-                }
-                
-            }
-            .navigationTitle("iExpense")
-            .toolbar {
-                
-                Button("Add expense", systemImage: "plus"){
-                    showingAddExpense = true
-                }
-                Button("Filter", systemImage: "line.3.horizontal.decrease.circle"){
-                    print("filter")
-                    sortOrder = [SortDescriptor(\ExpenseItem.amount)]
-                    print(sortOrder)
-                }
-                
-            }
-            .sheet(isPresented: $showingAddExpense, content: {
-                AddView()
-            })
+                .sheet(isPresented: $showingAddExpense, content: {
+                    AddView()
+                })
         }
     }
+
     
-    init(sortOrder: [SortDescriptor<ExpenseItem>]){
-        _expenses = Query(sort: sortOrder)
-    }
-    
-    func removeItems(at offsets: IndexSet) {
-        for offset in offsets {
-            // find this book in our query
-            let item = expenses[offset]
-            
-            // delete it from the context
-            modelContext.delete(item)
-        }
-    }
+
 }
 
 #Preview {
---
+++ iExpenseSwiftData/ExpesnseListView.swift
@@ -0,0 +1,65 @@
+//
+//  ExpesnseListView.swift
+//  iExpenseSwiftData
+//
+//  Created by Henrieke Baunack on 12/22/23.
+//
+import SwiftData
+import SwiftUI
+
+struct ExpesnseListView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var expenses: [ExpenseItem]
+    init(sort: [SortDescriptor<ExpenseItem>]) {
+        _expenses = Query(sort: sort)
+    }
+    var body: some View {
+        List{
+            // don't need the id: \.id anymore because we use the Identifiable protocol, Swift knows there is a unique id
+            Section("Personal Expenses"){
+                ForEach(expenses){ item in
+                    if item.type == "Personal" {
+                        HStack{
+                            VStack(alignment: .leading) {
+                                Text(item.name).font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .fontWeight(item.amount < 10.0 ? .light: item.amount < 100 ? .bold : .heavy)
+                        }
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            Section("Business Expenses"){
+                ForEach(expenses){ item in
+                    if item.type == "Business"{
+                        HStack{
+                            VStack(alignment: .leading) {
+                                Text(item.name).font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .fontWeight(item.amount < 10.0 ? .light: item.amount < 100 ? .bold : .heavy)
+                        }
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our query
+            let item = expenses[offset]
+            // delete it from the context
+            modelContext.delete(item)
+        }
+    }
+}
+
+#Preview {
+    ExpesnseListView(sort: [SortDescriptor(\ExpenseItem.name)])
+}
